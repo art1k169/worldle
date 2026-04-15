e530e748c07afc50717bc81ff9d89b594c77c636
art1k169 <youtminecraft23art@gmail.com>
2026-04-15 23:14:58 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,89 +1,115 @@
 const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const server = http.createServer(app);
 
-app.use(express.static(__dirname));
+const io = new Server(server, {
+    cors: {
+        origin: "https://art1k169.github.io",
+        methods: ["GET", "POST"]
+    }
+});
 
-let rooms = {};
+const rooms = {};
 
 io.on('connection', (socket) => {
-    // Создание комнаты
     socket.on('createRoom', (data) => {
-        const code = Math.random().toString(36).substring(2, 7).toUpperCase();
-        rooms[code] = { players: [{ id: socket.id, name: data.name }], secretWord: null };
+        const code = Math.random().toString(36).substring(2, 8).toUpperCase();
+        rooms[code] = {
+            players: [{ id: socket.id, name: data.name }],
+            secretWord: null,
+            guesses: []
+        };
         socket.join(code);
         socket.emit('roomCreated', { code });
+        io.to(code).emit('playerJoined', rooms[code].players);
     });
 
-    // Вход в комнату
     socket.on('joinRoom', (data) => {
         const room = rooms[data.code];
         if (room && room.players.length < 2) {
             room.players.push({ id: socket.id, name: data.name });
             socket.join(data.code);
             io.to(data.code).emit('playerJoined', room.players);
-        } else { socket.emit('errorMsg', 'Комната не найдена или заполнена'); }
-    });
-
-    // Исправленная кнопка "Выйти"
-    socket.on('leaveRoom', (code) => {
-        socket.leave(code);
-        if (rooms[code]) {
-            rooms[code].players = rooms[code].players.filter(p => p.id !== socket.id);
-            if (rooms[code].players.length === 0) {
-                delete rooms[code];
-            } else {
-                io.to(code).emit('playerJoined', rooms[code].players);
-            }
+        } else {
+            socket.emit('errorMsg', 'Комната не найдена или заполнена');
         }
-        socket.emit('leftSuccess');
     });
 
-    // Старт игры и раздача ролей
     socket.on('startGame', (code) => {
         const room = rooms[code];
-        if (!room) return;
-        const masterIdx = Math.floor(Math.random() * 2);
-        room.players.forEach((p, i) => {
-            io.to(p.id).emit('initRole', { role: (i === masterIdx ? 'master' : 'guesser') });
-        });
+        if (room && room.players.length === 2) {
+            io.to(room.players[0].id).emit('initRole', { role: 'master' });
+            io.to(room.players[1].id).emit('initRole', { role: 'guesser' });
+        }
     });
 
-    // Установка секретного слова
     socket.on('setSecretWord', (data) => {
-        if (rooms[data.code]) {
-            rooms[data.code].secretWord = data.word.toUpperCase();
+        const room = rooms[data.code];
+        if (room) {
+            room.secretWord = data.word.toUpperCase();
             io.to(data.code).emit('wordIsReady');
         }
     });
 
-    // Синхронизация ввода букв для Хоста
     socket.on('typeSync', (data) => {
         socket.to(data.code).emit('typeSync', data);
     });
 
-    // Проверка попытки
     socket.on('submitGuess', (data) => {
         const room = rooms[data.code];
-        if (room && room.secretWord) {
-            const secret = room.secretWord;
-            const guess = data.guess.toUpperCase();
-            let res = Array(5).fill('absent');
-            let sMap = secret.split(''), gMap = guess.split('');
+        if (!room || !room.secretWord) return;
 
-            for (let i = 0; i < 5; i++) {
-                if (gMap[i] === sMap[i]) { res[i] = 'correct'; sMap[i] = null; gMap[i] = null; }
+        const guess = data.guess.toUpperCase();
+        const secret = room.secretWord;
+        const states = Array(5).fill('absent');
+        const secretArr = secret.split('');
+        const guessArr = guess.split('');
+
+        guessArr.forEach((char, i) => {
+            if (char === secretArr[i]) {
+                states[i] = 'correct';
+                secretArr[i] = null;
             }
-            for (let i = 0; i < 5; i++) {
-                if (gMap[i] !== null && sMap.includes(gMap[i])) {
-                    res[i] = 'present'; sMap[sMap.indexOf(gMap[i])] = null;
-                }
+        });
+
+        guessArr.forEach((char, i) => {
+            if (states[i] !== 'correct' && secretArr.includes(char)) {
+                states[i] = 'present';
+                secretArr[secretArr.indexOf(char)] = null;
             }
-            const win = res.every(s => s === 'correct');
-            io.to(data.code).emit('guessResult', { guess, states: res, senderId: socket.id, isWin: win });
+        });
+
+        io.to(data.code).emit('guessResult', {
+            guess,
+            states,
+            isWin: guess === secret,
+            senderId: socket.id
+        });
+    });
+
+    socket.on('leaveRoom', (code) => {
+        socket.leave(code);
+        socket.emit('leftSuccess');
+        if (rooms[code]) {
+            rooms[code].players = rooms[code].players.filter(p => p.id !== socket.id);
+            if (rooms[code].players.length === 0) delete rooms[code];
+            else io.to(code).emit('playerJoined', rooms[code].players);
+        }
+    });
+
+    socket.on('disconnect', () => {
+        for (const code in rooms) {
+            rooms[code].players = rooms[code].players.filter(p => p.id !== socket.id);
+            if (rooms[code].players.length === 0) delete rooms[code];
+            else io.to(code).emit('playerJoined', rooms[code].players);
         }
     });
 });
 
-http.listen(3000, () => console.log('Server: http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
